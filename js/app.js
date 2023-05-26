31b13c968c9ace9afe0aea2994717f3a2be55072
DaDoDev7 <dario.plastina@gmail.com>
2023-05-26 22:12:53 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-//live MATCHES
+// Funzione per creare una promessa di ritardo
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const live = Vue.createApp({
   data() {
@@ -6,7 +9,7 @@ const live = Vue.createApp({
       matches: []
     };
   },
-  mounted() {
+  async mounted() {
     const url = 'https://footapi7.p.rapidapi.com/api/matches/live';
     const options = {
       method: 'GET',
@@ -16,99 +19,113 @@ const live = Vue.createApp({
       }
     };
 
-    fetch(url, options)
-      .then(response => response.json())
-      .then(result => {
-        const matches = result.events.slice(0, 15);
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      const matches = result.events.slice(0, 10);
 
-        // Array di promesse per i fetch delle immagini delle squadre
-        const imagePromises = matches.map(match => {
-          if (match.homeTeam && match.homeTeam.id) {
-            const homeTeamId = match.homeTeam.id;
-            const homeImageUrl = `https://footapi7.p.rapidapi.com/api/team/${homeTeamId}/image`;
-            const awayTeamId = match.awayTeam.id;
-            const awayImageUrl = `https://footapi7.p.rapidapi.com/api/team/${awayTeamId}/image`;
+      for (let i = 0; i < matches.length; i++) {
+        const match = matches[i];
 
-            const homeImagePromise = fetch(homeImageUrl, options)
-              .then(response => response.json())
-              .then(imageResult => {
-                match.homeTeam.image = imageResult.image;
-              })
-              .catch(error => {
-                console.error(error);
-              });
+        if (match.homeTeam && match.homeTeam.id) {
+          const homeTeamId = match.homeTeam.id;
+          const homeImageUrl = 'https://footapi7.p.rapidapi.com/api/team/' + homeTeamId + '/image';
+          const awayTeamId = match.awayTeam.id;
+          const awayImageUrl = 'https://footapi7.p.rapidapi.com/api/team/' + awayTeamId + '/image';
 
-            const awayImagePromise = fetch(awayImageUrl, options)
-              .then(response => response.json())
-              .then(imageResult => {
-                match.awayTeam.image = imageResult.image;
-              })
-              .catch(error => {
-                console.error(error);
-              });
+          const homeResponse = await fetch(homeImageUrl, options);
+          const homeBlob = await homeResponse.blob();
+          match.homeTeam.image = URL.createObjectURL(homeBlob);
 
-            return Promise.all([homeImagePromise, awayImagePromise])
-              .then(() => match);
-          } else {
-            return match;
+          const awayResponse = await fetch(awayImageUrl, options);
+          const awayBlob = await awayResponse.blob();
+          match.awayTeam.image = URL.createObjectURL(awayBlob);
+
+          if (i < matches.length - 1) {
+            await delay(1000); // Ritardo di 1 secondo tra le richieste
           }
-        });
+        }
+      }
 
-        // Attendiamo che tutte le promesse di fetch delle immagini siano risolte
-        Promise.all(imagePromises)
-          .then(updatedMatches => {
-            this.matches = updatedMatches;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      this.matches = matches;
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
 
 live.mount('#live');
 
 
+
+
+
+
   
   
   
 
-//   SCHEDULES
+// Funzione per creare una promessa di ritardo
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const schedules = Vue.createApp({
-    data() {
-      return {
-        matches: []
-      };
-    },
-    mounted() {
-      const today = new Date();
-      const day = today.getDate() +1;
-      const month = today.getMonth() +1 ;
-      const year = today.getFullYear();
-      
-      const url = `https://footapi7.p.rapidapi.com/api/matches/${day}/${month}/${year}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '00f86281a9mshc24dc7075c5d773p1f9175jsn91f5d88b88b1',
-          'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
+  data() {
+    return {
+      matches: []
+    };
+  },
+  async mounted() {
+    const today = new Date();
+    const day = today.getDate() + 1;
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    const url = `https://footapi7.p.rapidapi.com/api/matches/${day}/${month}/${year}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '00f86281a9mshc24dc7075c5d773p1f9175jsn91f5d88b88b1',
+        'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      const matches = result.events.slice(0, 10);
+
+      for (let i = 0; i < matches.length; i++) {
+        const match = matches[i];
+
+        if (match.homeTeam && match.homeTeam.id) {
+          const homeTeamId = match.homeTeam.id;
+          const homeImageUrl = `https://footapi7.p.rapidapi.com/api/team/${homeTeamId}/image`;
+          const awayTeamId = match.awayTeam.id;
+          const awayImageUrl = `https://footapi7.p.rapidapi.com/api/team/${awayTeamId}/image`;
+
+          const homeResponse = await fetch(homeImageUrl, options);
+          const homeBlob = await homeResponse.blob();
+          match.homeTeam.image = URL.createObjectURL(homeBlob);
+
+          const awayResponse = await fetch(awayImageUrl, options);
+          const awayBlob = await awayResponse.blob();
+          match.awayTeam.image = URL.createObjectURL(awayBlob);
+
+          if (i < matches.length - 1) {
+            await delay(1000); // Ritardo di 1 secondo tra le richieste
+          }
         }
-      };
-  
-      fetch(url, options)
-        .then(response => response.json())
-        .then(result => {
-          this.matches = result.events.slice(0, 15);;
-        })
-        .catch(error => {
-          console.error(error);
-        });
+      }
+
+      this.matches = matches;
+    } catch (error) {
+      console.error(error);
     }
-  });
-  
-  schedules.mount('#schedules');
+  }
+});
+
+schedules.mount('#schedules');
+
   
--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+//Modal Window
+
+var matchElements = document.querySelectorAll('.match_ai');
+var modalWindow = document.querySelector('.modalwindow');
+var closeButton = document.querySelector('.closebtn');
+var overlay = document.querySelector('.overlay');
+
+matchElements.forEach(function(matchElement) {
+  matchElement.addEventListener('click', function() {
+    showModal();
+  });
+});
+
+closeButton.addEventListener('click', function() {
+  closeModal();
+});
+
+overlay.addEventListener('click', function() {
+  closeModal();
+});
+
+function showModal() {
+  modalWindow.style.display = 'block';
+  overlay.style.display = 'block';
+  document.body.classList.add('modal-open');
+}
+
+function closeModal() {
+  modalWindow.style.display = 'none';
+  overlay.style.display = 'none';
+  document.body.classList.remove('modal-open');
+}
+
+
+
 //burgermenu
 
 
@@ -65,6 +100,74 @@ window.addEventListener('load', function() {
     }, index * delay);
   });
 });
+
+
+//Particles 
+
+particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":1262.6362266116362}},"color":{"value":"#4ab5b9"},"shape":{"type":"circle","stroke":{"width":5,"color":"#000000"},"polygon":{"nb_sides":12},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":4,"random":false,"anim":{"enable":false,"speed":7.308694910712106,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.603412060865523,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
+
+//football scroll 
+
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.scrollY;
+  var rotation = scrollPosition * 360 / (document.documentElement.scrollHeight - window.innerHeight);
+
+  var scrollObject = document.querySelector('.football_img');
+  scrollObject.style.transform = 'rotate(' + rotation + 'deg)';
+});
+
+
+//writing inside modal
+
+
+var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
 
 
 
